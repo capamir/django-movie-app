5cd0996dbca0944353b61f328bc19c25d9f6eafd
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-12 19:35:28 +0330

    admin panel bug fixed

--- movies/admin.py
+++ movies/admin.py
@@ -1,27 +1,14 @@
 from django.contrib import admin
 from .models import Movie, Genre, Actor
 
-class GenreInline(admin.TabularInline):
-    """Inline admin for genres in Movie admin."""
-    model = Movie.genres.through
-    extra = 1
-    verbose_name = "Genre"
-    verbose_name_plural = "Genres"
 
-class ActorInline(admin.TabularInline):
-    """Inline admin for actors in Movie admin."""
-    model = Movie.actors.through
-    extra = 1
-    verbose_name = "Actor"
-    verbose_name_plural = "Actors"
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     """Admin interface for Movie model."""
-    list_display = ('title', 'rating', 'created_at', 'genre_list', 'actor_list')
+    list_display = ('title', 'rating', 'created_at',)
     list_filter = ('rating', 'created_at', 'genres', 'actors')
     search_fields = ('title', 'plot_summary')
-    inlines = [GenreInline, ActorInline]
     date_hierarchy = 'created_at'
     list_per_page = 20
 
